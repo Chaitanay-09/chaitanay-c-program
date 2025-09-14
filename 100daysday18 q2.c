134ce520537668e6fe28d98ee94b293eddee0f6d
chaitanay <chaitanaygautam@gmail.com>
2025-09-14 12:13:24 +0530

    Add files via upload
    100 days of code day 18 question2

---
+++ 100daysday18 q2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    int num1, num2;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    int hcf = gcd(num1, num2);
+    printf("%d\n", hcf);
+
+    return 0;
+}
