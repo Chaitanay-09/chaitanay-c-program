46a18d018ec328996bc4b2fd48cf9f1521e81b9d
chaitanay <chaitanaygautam@gmail.com>
2025-10-09 12:38:41 +0530

    Add files via upload
    programming in c assignment question1

---
+++ program1 practice.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int counter = 0;
+
+void increment() {
+    counter++;
+    printf("Counter after increment: %d\n", counter);
+}
+
+void decrement() {
+    counter--;
+    printf("Counter after decrement: %d\n", counter);
+}
+
+void display() {
+    printf("Current counter value: %d\n", counter);
+}
+
+int main() {
+    printf("Initial counter value: %d\n", counter);
+
+    increment();  
+    increment();  
+    decrement(); 
+    display();     
+
+    return 0;
+}
